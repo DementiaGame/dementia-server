c45961e76d758586986e8dc34ea62007b48159fc
euics <pjh575157@gmail.com>
2024-05-30 15:08:51 +0900

    Initial Commit

---
+++ src/test/java/synapse/dementia/DementiaApplicationTests.java
@@ -0,0 +1,13 @@
+package synapse.dementia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DementiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
