40849ec08f8efc1631ee984d87dd2d6ea1828d14
euics <pjh575157@gmail.com>
2024-06-12 00:41:20 +0900

    feat: Dashboard 웹 페이지 하나로 통합 (#18)

--- src/main/java/synapse/dementia/domain/logs/controller/LogsController.java
+++ src/main/java/synapse/dementia/domain/logs/controller/LogsController.java
@@ -6,31 +6,67 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 import synapse.dementia.domain.logs.domain.ApiErrorLogs;
 import synapse.dementia.domain.logs.domain.ApiSuccessLogs;
 import synapse.dementia.domain.logs.service.LogsService;
+import synapse.dementia.domain.users.domain.Users;
+import synapse.dementia.domain.users.repository.UsersRepository;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/v1/admin")
+@RequestMapping("/admin")
 public class LogsController {
 	private final LogsService logsService;
+	private final UsersRepository usersRepository;
 
 	@GetMapping()
-	public String adminDashBoard() {
+	public String getDashboard(Model model) {
+		List<Users> users = usersRepository.findAll();
+		model.addAttribute("users", users);
+
+		List<ApiSuccessLogs> successLogs = logsService.findAllSuccessLogs().stream()
+			.sorted((log1, log2) -> log2.getRequestTime().compareTo(log1.getRequestTime()))
+			.collect(Collectors.toList());
+		model.addAttribute("successLogs", successLogs);
+
+		List<ApiErrorLogs> errorLogs = logsService.findAllErrorLogs().stream()
+			.sorted((log1, log2) -> log2.getRequestTime().compareTo(log1.getRequestTime()))
+			.collect(Collectors.toList());
+		model.addAttribute("errorLogs", errorLogs);
+
 		return "admin/dashboard";
+	}
+
+	@GetMapping("/all-logs")
+	public String allLogs(Model model) {
+		List<Users> users = usersRepository.findAll();
+		model.addAttribute("users", users);
+
+		List<ApiSuccessLogs> successLogs = logsService.findAllSuccessLogs().stream()
+			.sorted((log1, log2) -> log2.getRequestTime().compareTo(log1.getRequestTime()))
+			.collect(Collectors.toList());
+		model.addAttribute("successLogs", successLogs);
+
+		List<ApiErrorLogs> errorLogs = logsService.findAllErrorLogs().stream()
+			.sorted((log1, log2) -> log2.getRequestTime().compareTo(log1.getRequestTime()))
+			.collect(Collectors.toList());
+		model.addAttribute("errorLogs", errorLogs);
+
+		return "admin/all-logs";
 	}
 
 	@GetMapping("/users")
 	public String getAllUsersLogs(Model model) {
-
+		List<Users> users = usersRepository.findAll();
+		model.addAttribute("users", users);
 		return "admin/users";
 	}
 
-	@GetMapping("/logs/success")
+	@GetMapping("/success-logs")
 	public String getAllSuccessLogs(Model model) {
 		List<ApiSuccessLogs> successLogs = logsService.findAllSuccessLogs().stream()
 			.sorted((log1, log2) -> log2.getRequestTime().compareTo(log1.getRequestTime()))
@@ -40,7 +76,7 @@ public class LogsController {
 		return "admin/success-logs";
 	}
 
-	@GetMapping("/logs/error")
+	@GetMapping("/error-logs")
 	public String getAllFailLogs(Model model) {
 		List<ApiErrorLogs> errorLogs = logsService.findAllErrorLogs().stream()
 			.sorted((log1, log2) -> log2.getRequestTime().compareTo(log1.getRequestTime()))
