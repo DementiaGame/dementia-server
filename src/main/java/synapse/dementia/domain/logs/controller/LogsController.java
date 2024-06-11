f6426a9dc4498a6065e915aae4023183bb77548f
euics <pjh575157@gmail.com>
2024-06-11 21:33:36 +0900

    feat: Add Basic DashBoard Controller (#18)
    - Admin DashBoard (초기화면)
    - API 호출 성공 로그 관리
    - API 호출 실패 로그 관리
    - 게임별 통계

--- src/main/java/synapse/dementia/domain/logs/controller/LogsController.java
+++ src/main/java/synapse/dementia/domain/logs/controller/LogsController.java
@@ -1,10 +1,12 @@
 package synapse.dementia.domain.logs.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 import synapse.dementia.domain.logs.domain.ApiErrorLogs;
@@ -13,20 +15,38 @@ import synapse.dementia.domain.logs.service.LogsService;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/v1/admin")
 public class LogsController {
 	private final LogsService logsService;
 
-	@GetMapping("/v1/admin/success")
-	public String getAllSuccessLogs(Model model) {
-		List<ApiSuccessLogs> successLogs = logsService.findAllSuccessLogs();
-		model.addAttribute("successLogs", successLogs);
-		return "successLogs";
+	@GetMapping()
+	public String adminDashBoard() {
+		return "admin/dashboard";
 	}
 
-	@GetMapping("/v1/admin/error")
+	@GetMapping("/users")
+	public String getAllUsersLogs(Model model) {
+
+		return "admin/users";
+	}
+
+	@GetMapping("/logs/success")
+	public String getAllSuccessLogs(Model model) {
+		List<ApiSuccessLogs> successLogs = logsService.findAllSuccessLogs().stream()
+			.sorted((log1, log2) -> log2.getRequestTime().compareTo(log1.getRequestTime()))
+			.collect(Collectors.toList());
+
+		model.addAttribute("successLogs", successLogs);
+		return "admin/success-logs";
+	}
+
+	@GetMapping("/logs/error")
 	public String getAllFailLogs(Model model) {
-		List<ApiErrorLogs> errorLogs = logsService.findAllErrorLogs();
+		List<ApiErrorLogs> errorLogs = logsService.findAllErrorLogs().stream()
+			.sorted((log1, log2) -> log2.getRequestTime().compareTo(log1.getRequestTime()))
+			.collect(Collectors.toList());
+
 		model.addAttribute("errorLogs", errorLogs);
-		return "errorLogs";
+		return "admin/error-logs";
 	}
 }
