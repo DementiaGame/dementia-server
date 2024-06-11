01f934b9072e238bfd5914c49b49b83b60439eae
이 다정 <vicpillon0319@gamil.com>
2024-06-11 15:37:33 +0900

    feat: Add UsersSignUpReq dto

--- src/main/java/synapse/dementia/domain/users/dto/request/UsersSignUpReq.java
+++ src/main/java/synapse/dementia/domain/users/dto/request/UsersSignUpReq.java
@@ -1,4 +1,19 @@
 package synapse.dementia.domain.users.dto.request;
 
-public class UsersSignUpReq {
+import synapse.dementia.domain.users.domain.Gender;
+import synapse.dementia.domain.users.domain.Role;
+import synapse.dementia.domain.users.domain.Users;
+
+public record UsersSignUpReq(
+	String nickName,
+	String password,
+	Integer birthYear,
+	Gender gender,
+	Boolean deleted,
+	Role role
+) {
+	public UsersSignUpReq(Users entity) {
+		this(entity.getNickName(), entity.getPassword(), entity.getBirthYear(), entity.getGender(), false,
+			Role.ROLE_USER);
+	}
 }
