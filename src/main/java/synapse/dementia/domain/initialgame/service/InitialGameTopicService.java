3f541c9b8f613e9cd81bca021ccb52cc129dc3c1
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-06-13 23:41:57 +0900

    feat: 프로젝트를 실행하자마자 db에 엑셀의 topic들을 담기도록하는 초기화코드 작성

--- src/main/java/synapse/dementia/domain/initialgame/service/InitialGameTopicService.java
+++ src/main/java/synapse/dementia/domain/initialgame/service/InitialGameTopicService.java
@@ -5,7 +5,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 import synapse.dementia.domain.initialgame.constant.InitialGameTopicConstants;
 import synapse.dementia.domain.initialgame.domain.InitialGameTopic;
 import synapse.dementia.domain.initialgame.repository.InitialTopicRepository;
@@ -18,40 +17,40 @@ import java.util.List;
 @Service
 public class InitialGameTopicService {
     private final InitialTopicRepository initialTopicRepository;
-    public InitialGameTopicService(InitialTopicRepository initialTopicRepository){
-        this.initialTopicRepository=initialTopicRepository;
+
+    public InitialGameTopicService(InitialTopicRepository initialTopicRepository) {
+        this.initialTopicRepository = initialTopicRepository;
     }
 
-    public List<InitialGameTopic> saveTopicsFromExcel(MultipartFile file) throws IOException{
-        List<String> topicsList=readExcelFile(file);
-        List<InitialGameTopic> saveTopics=new ArrayList<>();
+    public List<InitialGameTopic> saveTopicsFromExcel(InputStream inputStream) throws IOException {
+        List<String> topicsList = readExcelFile(inputStream);
+        List<InitialGameTopic> savedTopics = new ArrayList<>();
 
-        for(String topicName:topicsList){
+        for (String topicName : topicsList) {
             InitialGameTopic topic = new InitialGameTopic(topicName);
-            saveTopics.add(initialTopicRepository.save(topic));
+            savedTopics.add(initialTopicRepository.save(topic));
         }
-        return saveTopics;
+        return savedTopics;
     }
 
-    private List<String> readExcelFile(MultipartFile file) throws IOException{
-        List<String> topicsList=new ArrayList<>();
+    private List<String> readExcelFile(InputStream inputStream) throws IOException {
+        List<String> topicsList = new ArrayList<>();
 
-        try(InputStream inputStream=file.getInputStream();
-            XSSFWorkbook workbook=new XSSFWorkbook(inputStream)){
-            XSSFSheet sheet=workbook.getSheetAt(InitialGameTopicConstants.TOPIC_SHEET_NUMBER);
+        try (XSSFWorkbook workbook = new XSSFWorkbook(inputStream)) {
+            XSSFSheet sheet = workbook.getSheetAt(InitialGameTopicConstants.TOPIC_SHEET_NUMBER);
 
             Row row = sheet.getRow(InitialGameTopicConstants.TOPIC_START_ROW);
-            if(row!=null){
-                for(int index = InitialGameTopicConstants.TOPIC_START_COLUMN; index<=InitialGameTopicConstants.TOPIC_END_COLUMN; index++){
+            if (row != null) {
+                for (int index = InitialGameTopicConstants.TOPIC_START_COLUMN; index <= InitialGameTopicConstants.TOPIC_END_COLUMN; index++) {
                     Cell cell = row.getCell(index);
-                    if(cell!=null){
-                        String value="";
-                        switch(cell.getCellType()){
+                    if (cell != null) {
+                        String value = "";
+                        switch (cell.getCellType()) {
                             case STRING:
-                                value=cell.getStringCellValue();
+                                value = cell.getStringCellValue();
                                 break;
                             case NUMERIC:
-                                value=String.valueOf(cell.getNumericCellValue());
+                                value = String.valueOf(cell.getNumericCellValue());
                                 break;
                             case BOOLEAN:
                                 value = String.valueOf(cell.getBooleanCellValue());
