ab99168146cc32aade1614e870921405ad0c2b6c
이 다정 <vicpillon0319@gamil.com>
2024-06-13 20:30:08 +0900

    feat: Update UserSignUpReq dto

--- src/main/java/synapse/dementia/domain/users/dto/request/UsersSignUpReq.java
+++ src/main/java/synapse/dementia/domain/users/dto/request/UsersSignUpReq.java
@@ -1,19 +1,35 @@
 package synapse.dementia.domain.users.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import synapse.dementia.domain.users.domain.Gender;
 import synapse.dementia.domain.users.domain.Role;
-import synapse.dementia.domain.users.domain.Users;
 
 public record UsersSignUpReq(
+	@NotBlank(message = "닉네임을 입력해주세요.")
+	@Pattern(regexp = "^(?![ㄱ-ㅎ])[a-zA-Z0-9가-힣]{2,12}$\n", message = "닉네임은 2~12자리면서 초성이 아닌 한글, 영어, 숫자로 구성되어야 합니다.")
 	String nickName,
+
+	@NotBlank(message = "비밀번호를 입력해주세요.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$", message = "비밀번호는 8~16자리면서 알파벳, 숫자, 특수문자를 포함해야 합니다.")
 	String password,
+
+	@NotBlank(message = "재확인 비밀번호를 입력해주세요.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$", message = "비밀번호는 8~16자리면서 알파벳, 숫자, 특수문자를 포함해야 합니다.")
+	String secondPassword,
+
+	@NotBlank(message = "출생연도를 입력해주세요.")
 	Integer birthYear,
+
+	@NotBlank(message = "성별을 입력해주세요.")
 	Gender gender,
 	Boolean deleted,
 	Role role
 ) {
-	public UsersSignUpReq(Users entity) {
-		this(entity.getNickName(), entity.getPassword(), entity.getBirthYear(), entity.getGender(), false,
-			Role.ROLE_USER);
+	public UsersSignUpReq {
+		if (deleted == null)
+			deleted = false;
+		if (role == null)
+			role = Role.ROLE_USER;
 	}
 }
