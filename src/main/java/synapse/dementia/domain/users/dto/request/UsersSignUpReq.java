14233660050b26fa9c0dfc0326cccc5ff5f4e07d
이 다정 <vicpillon0319@gamil.com>
2024-06-14 13:31:22 +0900

    feat: UsersSignUpReq nickName 정규식 수정

--- src/main/java/synapse/dementia/domain/users/dto/request/UsersSignUpReq.java
+++ src/main/java/synapse/dementia/domain/users/dto/request/UsersSignUpReq.java
@@ -8,7 +8,7 @@ import synapse.dementia.domain.users.domain.Role;
 
 public record UsersSignUpReq(
 	@NotBlank(message = "닉네임을 입력해주세요.")
-	@Pattern(regexp = "^(?![ㄱ-ㅎ])[a-zA-Z0-9가-힣]{2,12}$\n", message = "닉네임은 2~12자리면서 초성이 아닌 한글, 영어, 숫자로 구성되어야 합니다.")
+	@Pattern(regexp = "^(?![ㄱ-ㅎ])[a-zA-Z0-9가-힣]{2,12}$", message = "닉네임은 2~12자리면서 초성이 아닌 한글, 영어, 숫자로 구성되어야 합니다.")
 	String nickName,
 
 	@NotBlank(message = "비밀번호를 입력해주세요.")
