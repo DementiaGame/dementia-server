6b24cd1f1367f60925847e30766712fe545d0885
이 다정 <vicpillon0319@gamil.com>
2024-06-14 13:34:42 +0900

    feat: UsersSignUpRes dto 및 fromEntity 추가

--- src/main/java/synapse/dementia/domain/users/dto/response/UsersSignUpRes.java
+++ src/main/java/synapse/dementia/domain/users/dto/response/UsersSignUpRes.java
@@ -1,4 +1,15 @@
 package synapse.dementia.domain.users.dto.response;
 
-public class UsersSignUpRes {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import synapse.dementia.domain.users.domain.Users;
+
+public record UsersSignUpRes(
+	@NotBlank()
+	@Pattern(regexp = "^(?![ㄱ-ㅎ])[a-zA-Z0-9가-힣]{2,12}$\n", message = "닉네임은 2~12자리면서 초성이 아닌 한글, 영어, 숫자로 구성되어야 합니다.")
+	String nickName
+) {
+	public static UsersSignUpRes fromEntity(Users users) {
+		return new UsersSignUpRes(users.getNickName());
+	}
 }
