4128455ac038adc5788691047e7d078e1b1de4d0
이 다정 <vicpillon0319@gamil.com>
2024-06-14 15:37:12 +0900

    feat: MismatchPasswordException 생성

---
+++ src/main/java/synapse/dementia/global/exception/MismatchPasswordException.java
@@ -0,0 +1,10 @@
+package synapse.dementia.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class MismatchPasswordException extends RuntimeException {
+	private final ErrorResult errorResult;
+}
