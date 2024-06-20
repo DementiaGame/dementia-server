ec507826408b86b708322e6f87daa9e1e38871b3
euics <pjh575157@gmail.com>
2024-06-20 17:26:57 +0900

    feat: saveErrorLogs 메서드 logsService 클래스로 분리 (#32)

--- src/main/java/synapse/dementia/global/exception/GlobalExceptionHandler.java
+++ src/main/java/synapse/dementia/global/exception/GlobalExceptionHandler.java
@@ -1,11 +1,6 @@
 package synapse.dementia.global.exception;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.time.LocalDateTime;
-import java.util.Enumeration;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
@@ -24,15 +19,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import synapse.dementia.domain.logs.domain.ApiErrorLogs;
-import synapse.dementia.domain.logs.repository.ErrorLogsRepository;
+import synapse.dementia.domain.logs.service.LogsService;
 
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-	private final ErrorLogsRepository logsErrorRepository;
+	private final LogsService logsService;
 
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -46,17 +40,26 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 			.collect(Collectors.toList());
 
 		log.warn("Invalid DTO Parameter errors : {}", errorList);
-		saveErrorLog((HttpServletRequest)request, HttpStatus.BAD_REQUEST.value(), errorList.toString());
+		logsService.saveErrorLogs(
+			(HttpServletRequest)request,
+			HttpStatus.BAD_REQUEST.value(),
+			errorList.toString()
+		);
 		return makeErrorResponseEntity(errorList.toString());
 	}
 
 	@ExceptionHandler({ServerException.class})
 	public ResponseEntity<ErrorResponse> handleServerException(final ServerException exception,
 		HttpServletRequest request, HttpServletResponse response) {
+
 		log.warn("ServerException occur: ", exception);
-		saveErrorLog(request, exception.getErrorResult().getHttpStatus().value(),
-			exception.getErrorResult().getMessage());
+		logsService.saveErrorLogs(
+			request,
+			exception.getErrorResult().getHttpStatus().value(),
+			exception.getErrorResult().getMessage()
+		);
 		response.setStatus(exception.getErrorResult().getHttpStatus().value());
+
 		return makeErrorResponseEntity(exception.getErrorResult());
 	}
 
@@ -64,8 +67,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<ErrorResponse> handleException(final Exception exception, HttpServletRequest request,
 		HttpServletResponse response) {
 		log.warn("Exception occur: ", exception);
-		saveErrorLog(request, HttpStatus.INTERNAL_SERVER_ERROR.value(), ErrorResult.UNKNOWN_EXCEPTION.getMessage());
+		logsService.saveErrorLogs(
+			request,
+			HttpStatus.INTERNAL_SERVER_ERROR.value(),
+			ErrorResult.UNKNOWN_EXCEPTION.getMessage()
+		);
 		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
 		return makeErrorResponseEntity(ErrorResult.UNKNOWN_EXCEPTION);
 	}
 
@@ -73,9 +81,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<ErrorResponse> handleNotFoundException(final NotFoundException exception,
 		HttpServletRequest request, HttpServletResponse response) {
 		log.warn("NotFoundException occur: ", exception);
-		saveErrorLog(request, exception.getErrorResult().getHttpStatus().value(),
-			exception.getErrorResult().getMessage());
+		logsService.saveErrorLogs(
+			request,
+			exception.getErrorResult().getHttpStatus().value(),
+			exception.getErrorResult().getMessage()
+		);
 		response.setStatus(exception.getErrorResult().getHttpStatus().value());
+
 		return makeErrorResponseEntity(exception.getErrorResult());
 	}
 
@@ -83,9 +95,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<ErrorResponse> handleBadRequestException(final BadRequestException exception,
 		HttpServletRequest request, HttpServletResponse response) {
 		log.warn("ConflictException occur: ", exception);
-		saveErrorLog(request, exception.getErrorResult().getHttpStatus().value(),
-			exception.getErrorResult().getMessage());
+		logsService.saveErrorLogs(
+			request,
+			exception.getErrorResult().getHttpStatus().value(),
+			exception.getErrorResult().getMessage()
+		);
 		response.setStatus(exception.getErrorResult().getHttpStatus().value());
+
 		return makeErrorResponseEntity(exception.getErrorResult());
 	}
 
@@ -93,50 +109,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<ErrorResponse> handleConflictException(final ConflictException exception,
 		HttpServletRequest request, HttpServletResponse response) {
 		log.warn("ConflictException occur: ", exception);
-		saveErrorLog(request, exception.getErrorResult().getHttpStatus().value(),
-			exception.getErrorResult().getMessage());
+		logsService.saveErrorLogs(
+			request,
+			exception.getErrorResult().getHttpStatus().value(),
+			exception.getErrorResult().getMessage()
+		);
 		response.setStatus(exception.getErrorResult().getHttpStatus().value());
+
 		return makeErrorResponseEntity(exception.getErrorResult());
-	}
-
-	// <-- Exception 정의
-
-	private void saveErrorLog(HttpServletRequest request, int status, String errorMessage) {
-		String serverIp = getServerIp().orElse("Unknown");
-		long start = System.currentTimeMillis();
-		long end = System.currentTimeMillis();
-		ApiErrorLogs logError = ApiErrorLogs.builder()
-			.serverIp(serverIp)
-			.requestURL(request.getRequestURL().toString())
-			.requestMethod(request.getMethod())
-			.responseStatus(status)
-			.clientIp(request.getRemoteAddr())
-			.requestTime(LocalDateTime.now())
-			.responseTime(LocalDateTime.now().plusNanos(end - start))
-			.connectionTime(end - start)
-			.errorMessage(errorMessage)
-			.build();
-
-		logsErrorRepository.save(logError);
-	}
-
-	private Optional<String> getServerIp() {
-		try {
-			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-			while (interfaces.hasMoreElements()) {
-				NetworkInterface networkInterface = interfaces.nextElement();
-				Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
-				while (inetAddresses.hasMoreElements()) {
-					InetAddress inetAddress = inetAddresses.nextElement();
-					if (!inetAddress.isLoopbackAddress() && inetAddress.isSiteLocalAddress()) {
-						return Optional.of(inetAddress.getHostAddress());
-					}
-				}
-			}
-		} catch (Exception e) {
-			log.error("Error while getting server IP address", e);
-		}
-		return Optional.empty();
 	}
 
 	private ResponseEntity<Object> makeErrorResponseEntity(final String errorDescription) {
