23f1de90b3a7463fb92c0fd04e425d9dd70d8a6e
이 다정 <vicpillon0319@gamil.com>
2024-06-20 14:18:42 +0900

    feat: spring security session 기능 구현

---
+++ src/main/java/synapse/dementia/global/security/CustomAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package synapse.dementia.global.security;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StringUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import synapse.dementia.domain.auth.dto.request.UsersSignInReq;
+import synapse.dementia.global.exception.BadRequestException;
+import synapse.dementia.global.exception.ErrorResult;
+
+public class CustomAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+	private ObjectMapper objectMapper = new ObjectMapper();
+
+	public CustomAuthenticationFilter() {
+		super(new AntPathRequestMatcher("/auth/signIn", "POST"));
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws
+		AuthenticationException,
+		IOException,
+		ServletException {
+		UsersSignInReq usersSignInReq = objectMapper.readValue(request.getReader(), UsersSignInReq.class);
+
+		// ID, PASSWORD 가 있는지 확인
+		if (!StringUtils.hasLength(usersSignInReq.nickName())
+			|| !StringUtils.hasLength(usersSignInReq.password())) {
+			throw new BadRequestException(ErrorResult.DTO_Request_BAD_REQUEST);
+		}
+
+		CustomAuthenticationToken token = new CustomAuthenticationToken(
+			usersSignInReq.nickName(),
+			usersSignInReq.password()
+		);
+
+		Authentication authentication = getAuthenticationManager().authenticate(token);
+
+		logger.info("CustomAuthenticationFilter" + usersSignInReq.toString());
+
+		return authentication;
+	}
+
+	// @Override
+	// protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+	// 	FilterChain chain, Authentication authResult) throws IOException, ServletException {
+	// 	SecurityContextHolder.getContext().setAuthentication(authResult);
+	// 	response.setStatus(HttpServletResponse.SC_OK);
+	// 	response.getWriter().write("{\"message\": \"Authentication successful\"}");
+	// }
+	//
+	// @Override
+	// protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+	// 	AuthenticationException failed) throws IOException, ServletException {
+	// 	response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+	// 	response.getWriter().write("{\"message\": \"Authentication failed\"}");
+	// }
+}
--- src/main/java/synapse/dementia/global/security/SecurityConfig.java
+++ src/main/java/synapse/dementia/global/security/SecurityConfig.java
@@ -1,18 +1,46 @@
 package synapse.dementia.global.security;
 
+import java.util.Collections;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
+
+	private final AuthenticationConfiguration authenticationConfiguration;
+	private final CustomAuthenticationSuccessHandler authenticationSuccessHandler;
+	private final CustomAuthenticationFailureHandler authenticationFailureHandler;
+	private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+	private final CustomAccessDeniedHandler accessDeniedHandler;
+
+	public SecurityConfig(AuthenticationConfiguration authenticationConfiguration,
+		CustomAuthenticationSuccessHandler authenticationSuccessHandler,
+		CustomAuthenticationFailureHandler authenticationFailureHandler,
+		CustomAuthenticationEntryPoint authenticationEntryPoint, CustomAccessDeniedHandler accessDeniedHandler) {
+		this.authenticationConfiguration = authenticationConfiguration;
+		this.authenticationSuccessHandler = authenticationSuccessHandler;
+		this.authenticationFailureHandler = authenticationFailureHandler;
+		this.authenticationEntryPoint = authenticationEntryPoint;
+		this.accessDeniedHandler = accessDeniedHandler;
+	}
 
 	@Bean
 	@ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
@@ -21,24 +49,90 @@ public class SecurityConfig {
 			.requestMatchers(PathRequest.toH2Console());
 	}
 
+	//  CORS 설정
+	CorsConfigurationSource corsConfigurationSource() {
+		return request -> {
+			CorsConfiguration config = new CorsConfiguration();
+			config.setAllowedHeaders(Collections.singletonList("*"));
+			config.setAllowedMethods(Collections.singletonList("*"));
+			config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:3000")); // 허용할 origin
+			config.setAllowCredentials(true);
+			return config;
+		};
+	}
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		// TODO: ADMIN ROLE 조건 추가
+
 		http
+			.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
 			.csrf(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
+			// .rememberMe(r -> r
+			// 	.rememberMeParameter("remember")
+			// 	.tokenValiditySeconds(3600)
+			// 	.alwaysRemember(false))
+			.headers(httpSecurityHeadersConfigurer ->
+				httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer
+					.FrameOptionsConfig::sameOrigin)
+			)
 			.authorizeHttpRequests(authorize -> authorize
-				.requestMatchers("/**").permitAll()
+				.requestMatchers("/", "/users/signUp", "/auth/signIn").permitAll()
+				// .requestMatchers("/admin/**").hasRole(Role.ROLE_ADMIN.name())
 				.anyRequest().authenticated())
-			.sessionManagement(session ->
-				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+			.addFilterBefore(
+				ajaxAuthenticationFilter(),
+				UsernamePasswordAuthenticationFilter.class)
+			.sessionManagement(session -> session
+					.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+					.sessionFixation((sessionFixation) -> sessionFixation.newSession())
+					.maximumSessions(1)
+					.maxSessionsPreventsLogin(true)
+				//.sessionRegistry()
+			)
+			.exceptionHandling(config -> config
+				.authenticationEntryPoint(authenticationEntryPoint)
+				.accessDeniedHandler(accessDeniedHandler));
 
 		return http.build();
 	}
 
+	// @Bean
+	// public AuthenticationManager authenticationManager(
+	// 	UserDetailsService userDetailsService,
+	// 	PasswordEncoder passwordEncoder
+	// ) {
+	// 	DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+	// 	authenticationProvider.setUserDetailsService(userDetailsService);
+	// 	authenticationProvider.setPasswordEncoder(passwordEncoder);
+	//
+	// 	return new ProviderManager(authenticationProvider);
+	// }
+
 	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+	public CustomAuthenticationFilter ajaxAuthenticationFilter() throws Exception {
+		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter();
+		customAuthenticationFilter.setAuthenticationManager(customAuthenticationManager());
+		customAuthenticationFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler);
+		customAuthenticationFilter.setAuthenticationFailureHandler(authenticationFailureHandler);
+
+		customAuthenticationFilter.setSecurityContextRepository(
+			new DelegatingSecurityContextRepository(
+				new RequestAttributeSecurityContextRepository(),
+				new HttpSessionSecurityContextRepository()
+			));
+
+		return customAuthenticationFilter;
+	}
+
+	@Bean
+	public AuthenticationManager customAuthenticationManager() throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() throws Exception {
 		return new BCryptPasswordEncoder();
 	}
 
