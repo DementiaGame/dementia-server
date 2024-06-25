ed62b9c2f0911c6ff7ee3c854a1e44f4c10ab7bf
euics <pjh575157@gmail.com>
2024-06-25 18:23:48 +0900

    feat: Change List to Page (#38)

--- src/main/java/synapse/dementia/domain/admin/logs/repository/ErrorLogsRepository.java
+++ src/main/java/synapse/dementia/domain/admin/logs/repository/ErrorLogsRepository.java
@@ -2,23 +2,24 @@ package synapse.dementia.domain.admin.logs.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import synapse.dementia.domain.admin.logs.domain.ApiErrorLogs;
-import synapse.dementia.domain.admin.logs.domain.ApiSuccessLogs;
 
 @Repository
 public interface ErrorLogsRepository extends JpaRepository<ApiErrorLogs, Long> {
 	@Query("""
-			SELECT 
-				a
-			FROM 
-				ApiErrorLogs a
-			ORDER BY 
-				a.requestTime
-			DESC
-			""")
-	List<ApiErrorLogs> findApiErrorLogsDesc();
+		SELECT 
+			a
+		FROM 
+			ApiErrorLogs a
+		ORDER BY 
+			a.requestTime
+		DESC
+		""")
+	Page<ApiErrorLogs> findApiErrorLogsDesc(Pageable pageable);
 }
--- src/main/java/synapse/dementia/domain/admin/logs/service/LogsService.java
+++ src/main/java/synapse/dementia/domain/admin/logs/service/LogsService.java
@@ -3,15 +3,18 @@ package synapse.dementia.domain.admin.logs.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import jakarta.servlet.http.HttpServletRequest;
 import synapse.dementia.domain.admin.logs.domain.ApiErrorLogs;
 import synapse.dementia.domain.admin.logs.domain.ApiSuccessLogs;
 import synapse.dementia.domain.admin.config.logs.CustomHttpServletResponseWrapper;
 
 public interface LogsService {
-	List<ApiErrorLogs> findAllErrorLogs();
+	Page<ApiErrorLogs> findAllErrorLogs(Pageable pageable);
 
-	List<ApiSuccessLogs> findAllSuccessLogs();
+	Page<ApiSuccessLogs> findAllSuccessLogs(Pageable pageable);
 
 	void saveSuccessLogs(
 		HttpServletRequest request,
