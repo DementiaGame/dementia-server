9f3f8146ce292211b921194eff5919ecc26d7a52
euics <pjh575157@gmail.com>
2024-06-25 17:37:46 +0900

    feat: RequestTime 정렬 수정 (#45)
    - 서버단 sort 처리 -> DB 쿼리문으로 수정

--- src/main/java/synapse/dementia/domain/admin/logs/repository/SuccessLogsRepository.java
+++ src/main/java/synapse/dementia/domain/admin/logs/repository/SuccessLogsRepository.java
@@ -1,10 +1,23 @@
 package synapse.dementia.domain.admin.logs.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import synapse.dementia.domain.admin.logs.domain.ApiSuccessLogs;
 
 @Repository
 public interface SuccessLogsRepository extends JpaRepository<ApiSuccessLogs, Long> {
+	@Query("""
+			SELECT 
+				a
+			FROM 
+				ApiSuccessLogs a
+			ORDER BY 
+				a.requestTime
+			DESC
+			""")
+	List<ApiSuccessLogs> findApiSuccessLogsDesc();
 }
