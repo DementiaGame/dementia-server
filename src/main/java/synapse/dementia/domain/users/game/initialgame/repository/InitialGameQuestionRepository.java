5011c4243de34d7ad5b2672b55592afa47267e92
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-06-26 16:53:43 +0900

    refactor: 한 사용자가 초성 게임을 여러번 할 수 있도록 수정

--- src/main/java/synapse/dementia/domain/users/game/initialgame/repository/InitialGameQuestionRepository.java
+++ src/main/java/synapse/dementia/domain/users/game/initialgame/repository/InitialGameQuestionRepository.java
@@ -2,8 +2,11 @@ package synapse.dementia.domain.users.game.initialgame.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import synapse.dementia.domain.users.game.initialgame.domain.InitialGameQuestion;
+import synapse.dementia.domain.users.game.initialgame.domain.SelectedGameTopic;
 import synapse.dementia.domain.users.member.domain.Users;
 
 public interface InitialGameQuestionRepository extends JpaRepository<InitialGameQuestion, Long> {
     int countByUserAndCorrect(Users user, boolean correct);
+    void deleteBySelectedGameTopic(SelectedGameTopic selectedGameTopic);
+
 }
