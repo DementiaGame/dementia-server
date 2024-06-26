91948d9a6d9e1c665bc64580b9b206d5e905e20f
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-06-26 17:36:55 +0900

    refactor: 결과repository 추가

--- src/main/java/synapse/dementia/domain/users/member/repository/UsersRepository.java
+++ src/main/java/synapse/dementia/domain/users/member/repository/UsersRepository.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 import synapse.dementia.domain.users.member.domain.Users;
 
+@Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
 	Optional<Users> findByNickName(String nickName);
 }
