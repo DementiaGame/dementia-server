b296085316bcce351cd05bfda912aec54918420c
euics <pjh575157@gmail.com>
2024-06-27 23:00:37 +0900

    feat: Add deleteUsers modifyUsers Interface Method (#49)
    - deleteUsers()
      - 회원 정보 삭제
    - modifyUsers()
      - 회원 정보 수정

--- src/main/java/synapse/dementia/domain/admin/member/service/MemberService.java
+++ src/main/java/synapse/dementia/domain/admin/member/service/MemberService.java
@@ -3,7 +3,13 @@ package synapse.dementia.domain.admin.member.service;
 import java.util.List;
 
 import synapse.dementia.domain.admin.member.dto.MemberDto;
+import synapse.dementia.domain.admin.member.dto.request.DeleteMemberDto;
+import synapse.dementia.domain.admin.member.dto.request.ModifyMemberDto;
 
 public interface MemberService {
 	List<MemberDto> findAllUsers();
+
+	void deleteUsers(DeleteMemberDto deleteMemberDto);
+
+	void modifyUsers(ModifyMemberDto modifyMemberDto);
 }
