09895cbe288284a5888fabf854f8d071a35b24c6
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-06-27 16:09:12 +0900

    refactor: 중복 주제더라도 문제 생성되도록 수정

--- src/main/java/synapse/dementia/domain/users/game/initialgame/repository/SelectedGameTopicRepository.java
+++ src/main/java/synapse/dementia/domain/users/game/initialgame/repository/SelectedGameTopicRepository.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Repository;
 import synapse.dementia.domain.users.game.initialgame.domain.SelectedGameTopic;
 import synapse.dementia.domain.users.member.domain.Users;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SelectedGameTopicRepository extends JpaRepository<SelectedGameTopic, Long> {
-    Optional<SelectedGameTopic> findByUserAndTopicName(Users user, String topicName);
     Optional<SelectedGameTopic> findByUser(Users user);
+    List<SelectedGameTopic> findByUserAndTopicName(Users user, String topicName);
 
 }
